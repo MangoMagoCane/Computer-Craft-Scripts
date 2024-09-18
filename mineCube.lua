e7f3eaeb209da12fa1b115c9b85b3382e58fafb2
Leonardo Garman <sir.mangocane@gmail.com>
2024-09-17 22:59:13 -0400

    feat: Add horz movement arg

--- mineCube.lua
+++ mineCube.lua
@@ -1,17 +1,26 @@
--- [uint col, uint row, uint depth, nil|"R"|"r" dir]
+-- [uint col, uint row, uint depth, nil|"R"|"r" horzDir, nil|"U"|"u" vertDir]
 local args = { ... }
 local col = tonumber(args[1])
 local row = tonumber(args[2])
 local depth = tonumber(args[3])
-local dir = args[4]
+local horzDir = args[4]
+local vertDir = args[5]
 
-local turnDir1, turnDir2;
-if (dir == "R" or dir == "r") then
+local turnDir1, turnDir2, vertDig, vertMove;
+if (horzDir == "R" or horzDir == "r") then
     turnDir1 = turtle.turnRight
     turnDir2 = turtle.turnLeft
 else
     turnDir1 = turtle.turnLeft
     turnDir2 = turtle.turnRight
+end
+
+if (vertDir == "U" or vertDir == "u") then
+    vertDig = turtle.digUp
+    vertMove = turtle.up
+else
+    vertDig = turtle.digDown
+    vertMove = turtle.down
 end
 
 function main()
@@ -60,8 +69,8 @@ function mineLayer(col, row)
     end
 
     turnDir1()
-    turtle.digDown()
-    turtle.down()
+    turtle.vertDig()
+    turtle.vertMove()
 end
 
 main()
