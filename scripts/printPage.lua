0f89f27454a57c6dcf132500b89a051c005cd4c5
EvanSadler7 <esadler18@yahoo.com>
2024-09-19 01:13:34 -0400

    Create printPage.lua
    Super basic printing function

---
+++ scripts/printPage.lua
@@ -0,0 +1,16 @@
+-- Version 1.0
+-- Usage: printPage [string to be printed on paper] [side of computer printer is touching]
+local args = { ... }
+local printer = peripheral.wrap("right")
+local printString = args[1]
+local wrap = args[2]
+
+if printer.newPage() then
+  if(printer.write(printString)) then
+    print("Successful write operation")
+  else print("Writing error!")
+  end
+else print("Print error!")
+end
+printer.endPage()
+
