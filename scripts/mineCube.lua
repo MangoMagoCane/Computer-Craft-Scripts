0e5fbd8b4dc1cf07af39549b46e33e3ac239fd79
Leonardo Garman <sir.mangocane@gmail.com>
2024-09-21 01:28:33 -0400

    fix: minecube refuel logic

--- scripts/mineCube.lua
+++ scripts/mineCube.lua
@@ -1,25 +1,64 @@
 -- VERSION 1.1
--- [uint col, uint row, uint depth, nil|"-r" horzDir, nil|"-u" vertDir, nil|"-c"|-"e"| chest]
+-- [uint col, uint row, uint depth,]
+-- ["-r" go right insead of left, "-u" go up instead of down, "-c", use a chest, "-e", use an enderchest]
+-- ! current bug in the refuel logic, should probably run if any movement logic fails
 local args = { ... }
 local col = tonumber(args[1])
 local row = tonumber(args[2])
 local depth = tonumber(args[3])
-local horzDir = args[4]
-local vertDir = args[5]
-local chest = args[6]
 
-local LAYER_TO_CHEST_COUNT = 2
+if (col == nil or row == nil or depth == nil) then
+  print("---------------------------------------")
+  print("usage: mineCube col row depth [options]")
+  print("optional parameters in any order:")
+  print("\t-r go right insead of left")
+  print('\t-u go up instead of down')
+  print("\t-c [dist] use a chest")
+  print("\t-ce use an ender chest")
+  print('example: mineCube 5 2 3 -r -u -c')
+  print('example: mineCube 3 3 3 -ce')
+  return;
+end
 
-local useChest, useEnder = false, false
-local turnDir1, turnDir2
+local goRight, goUp, useChest, useEnder, chestDistance = false, false, false, false, 0
 local vertDigForward, vertMoveForward, vertDigBackword, vertMoveBackword, vertDrop
+local turnDir1, turnDir2
 
 local log_layer = 1
 local log_col = 1
 local log_row = 1
 local log_blocksMined = 0
 
-if horzDir == "-r" then
+local i = 4
+while i <= #args do
+  print(i)
+  local currArg = args[i]
+  if currArg == "-r" then
+    goRight = true
+  elseif currArg == "-u" then
+    goUp = true
+  elseif currArg == "-r" then
+    goRight = true
+  elseif string.sub(currArg, 1, 2) == "-c" then
+    useChest = true
+    print("foo")
+    if string.sub(currArg, 3) == "e" then
+      useEnder = true
+    else
+      chestDistance = tonumber(args[i + 1]) or 0
+      if chestDistance ~= 0 then
+        print(chestDistance)
+        i = i + 1
+      end
+    end
+  else
+    print(currArg .. " is an invalid option")
+    return -1;
+  end
+  i = i + 1
+end
+
+if goRight then
   turnDir1 = turtle.turnRight
   turnDir2 = turtle.turnLeft
 else
@@ -27,7 +66,7 @@ else
   turnDir2 = turtle.turnRight
 end
 
-if vertDir == "-u" then
+if goUp then
   vertDigForward = turtle.digUp
   vertMoveForward = turtle.up
   vertDigBackword = turtle.digDown
@@ -41,12 +80,6 @@ else
   vertDrop = turtle.dropUp
 end
 
-if chest == '-c' then
-  useChest = true
-elseif chest == '-e' then
-  useChest = true
-  useEnder = true
-end
 
 function main()
   return mineLayers(col, row, depth)
@@ -105,30 +138,36 @@ function drop_items()
   end
 end
 
+function refuel()
+  local dotCount = 0
+  while turtle.getFuelLevel() == 0 do
+    for i = 1, 16 do
+      turtle.select(i)
+      turtle.refuel()
+    end
+    local dotStr = ""
+    for _ = 1, dotCount + 1 do
+      dotstr = dotStr .. "."
+    end
+    clearScreen()
+    print("err: turtle ran out of fuel, waiting to be refilled" .. dotStr)
+    dotCount = (dotCount + 1) % 3
+  end
+  clearScreen()
+  print("refuel success! continuing process")
+  turtle.select(1)
+end
+
 function mineLayers(col, row, depth)
   for i = 1, depth do
     log_layer = i
 
-    -- refuel logic
-    if turtle.getFuelLevel() == 0 then
-      local dotCount = 0
-      while (not turtle.refuel()) do
-        local dotStr = ""
-        for _ = 1, dotCount + 1 do
-          dotstr = dotStr .. "."
-        end
-        clearScreen()
-        print("err: turtle ran out of fuel, waiting to be refilled" .. dotStr)
-        dotCount = (dotCount + 1) % 3
-      end
-      clearScreen()
-      print("refuel success! continuing process")
-    end
 
     for j = 1, row do
       log_row = j
       for k = 1, col - 1 do
         log_col = k
+        refuel()
         digForward()
       end
 
@@ -164,19 +203,19 @@ function mineLayers(col, row, depth)
       break
     end
 
-    -- if useChest and i % LAYER_TO_CHEST_COUNT == 0 then
-    if useChest and turtle.getSelectedSlot(16) ~= 0 then
+    if useChest and turtle.getItemCount(15) ~= 0 then -- 15 slots full instead of 16 to minimize lost items
       clearScreen()
       if useEnder then
         print("DEPOSITING ITEMS IN ENDER CHEST")
       else
         print("RETURNING TO CHEST")
-        for _ = 1, i - 1 do
+        local distance = i + chestDistance - 1
+        for _ = 1, distance do
           vertDigBackword()
           vertMoveBackword()
         end
         drop_items()
-        for _ = 1, i - 1 do
+        for _ = 1, distance do
           vertDigForward()
           vertMoveForward()
         end
